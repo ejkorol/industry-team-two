09c8bf6e7776c5000582f8b1d77d6d6c085d333c
Jason Korol <ejkorol@gmail.com>
2024-06-28 14:27:54 -0400

    add informative text conditionally

--- src/components/ImageGen/ImageGen.jsx
+++ src/components/ImageGen/ImageGen.jsx
@@ -3,6 +3,7 @@ import { returnData } from "@/utils/vercelSDK";
 import Post from "@/components/Post/Post";
 import PostSkeleton from "@/components/PostSkeleton/PostSkeleton";
 import "./ImageGen.scss";
+import bulbIcon from "@/assets/lightbulb.svg";
 
 export default function ImageGen() {
 
@@ -50,6 +51,19 @@ export default function ImageGen() {
         <input className="upload__file-input" id="image" type="file" onChange={selectFile} accept="image/*" required />
         {imagePreview && <img className="upload__image" src={imagePreview} alt="work hard everyday, make few fucking money" />}
       </div>
+      <div className="image-label__wrapper">
+        {!imagePreview && <h3 className="image-label__text">Select an image that provides the best view of your item</h3> }
+        {imagePreview && <div className="image-label__wrapper--with-button">
+          <h3 className="image-label__text image-label__text--light">Do you want to add more photos?</h3>
+          <h3 className="image-label__text">add more</h3>
+        </div> }
+      </div>
+      {imagePreview && 
+        <div className="informative">
+          <img className="informative__icon" alt="icon" src={bulbIcon} />
+          <p className="informative__text">Our generative text uses popular keywords to ensure your listing reaches the right audience.</p>
+        </div>
+      }
       <br />
       {base64 && !data && <PostSkeleton />}
       {data && <Post analysis={data.analysis} />}
