5963bbcb492ea8ae193c1bfbec7beca988de2b6d
Jason Korol <ejkorol@gmail.com>
2024-06-28 02:06:35 -0400

    integrate image gen component into home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
 import Home from "@/components/Home/Home";
 import NavBar from "@/components/NavBar/NavBar";
+import ImageGen from "@/components/ImageGen/ImageGen";
 
 export default function HomePage() {
   return (
     <>
       <NavBar />
-      <Home />
+      <ImageGen />
     </>
   );
 };
