126926bc4987c116c8036bd0e6b33b224213cb9f
Jason Korol <ejkorol@gmail.com>
2024-06-27 21:15:48 -0400

    import navbar into home

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,10 @@
 import Home from "@/components/Home/Home";
+import NavBar from "@/components/NavBar/NavBar";
 
 export default function HomePage() {
   return (
     <>
+      <NavBar />
       <Home />
     </>
   );
