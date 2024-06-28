230f658f0e53de0020dfcb65e3b8bc66114181dd
Eo Soo Kim <eosookim@gmail.com>
2024-06-28 13:37:45 -0400

    change remove tag button

--- src/components/SingleTag/SingleTag.jsx
+++ src/components/SingleTag/SingleTag.jsx
@@ -4,9 +4,9 @@ export default function SingleTag({tag, index, removeTag}) {
   return (
     <div className="tag">
       <p className="tag__text">{tag}</p>
-      <button className="tag__remove">
+      <p className="tag__remove">
         X
-      </button>
+      </p>
     </div>
   );
 }
