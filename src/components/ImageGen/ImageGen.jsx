47901eb759dbc8883a50ce65fe74733b5b932917
Jason Korol <ejkorol@gmail.com>
2024-06-28 02:25:52 -0400

    add image preview

--- src/components/ImageGen/ImageGen.jsx
+++ src/components/ImageGen/ImageGen.jsx
@@ -1,14 +1,28 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { returnData } from "@/utils/vercelSDK";
+import "./ImageGen.scss";
 
 export default function ImageGen() {
 
   const [ selectedFile, setSelectedFile ] = useState('');
   const [ base64, setBase64 ] = useState('');
   const [ data, setData ] = useState(null);
+  const [ imagePreview, setImagePreview ] = useState(null);
+  const [ image, setImage ] = useState(null);
 
   const selectFile = (e) => {
     setSelectedFile(e.target.files[0])
+    const imageFile = e.target.files[0];
+    setImage(imageFile)
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImagePreview(reader.result);
+    };
+    if (imageFile) {
+      reader.readAsDataURL(imageFile);
+    } else {
+      setImagePreview(null);
+    };
   };
 
   const convertBase = () => {
@@ -20,14 +34,19 @@ export default function ImageGen() {
       setBase64(base64String);
       const returnedData = await returnData(base64String);
       setData(returnedData);
-      console.log(returnedData);
     }
   }
 
   return (
     <>
       <h1>Image Generation</h1>
-      <input type='file' id='image' name='upload image' onChange={selectFile} />
+      <br/>
+      <div className="upload__image-wrapper">
+        { !imagePreview && <label htmlFor="image" className="upload__file-input-label">select an image</label> }
+        <input className="upload__file-input" id="image" type="file" onChange={selectFile}  accept="image/*" required />
+        { imagePreview && <img className="upload__image" src={imagePreview} alt="work hard everyday, make few fucking money" /> }
+      </div>
+      <br/>
       <button onClick={() => { convertBase() }}>convert</button>
       {data && <>
         <h1>{data.analysis.title}</h1>
