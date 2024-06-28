995669aea4901fcc5a2c94656450df567164a34e
Jason Korol <ejkorol@gmail.com>
2024-06-28 13:39:18 -0400

    change button to image handler

--- src/components/ImageGen/ImageGen.jsx
+++ src/components/ImageGen/ImageGen.jsx
@@ -6,48 +6,50 @@ import "./ImageGen.scss";
 
 export default function ImageGen() {
 
-  const [ selectedFile, setSelectedFile ] = useState('');
-  const [ base64, setBase64 ] = useState('');
-  const [ data, setData ] = useState(null);
-  const [ imagePreview, setImagePreview ] = useState(null);
-  const [ image, setImage ] = useState(null);
+  const [selectedFile, setSelectedFile] = useState('');
+  const [base64, setBase64] = useState('');
+  const [data, setData] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null);
+  const [image, setImage] = useState(null);
 
   const selectFile = (e) => {
-    setSelectedFile(e.target.files[0])
     const imageFile = e.target.files[0];
-    setImage(imageFile)
+    setSelectedFile(imageFile);
+    setImage(imageFile);
+
     const reader = new FileReader();
     reader.onloadend = () => {
       setImagePreview(reader.result);
+      // Automatically trigger convertBase when the image is selected
+      convertBase(imageFile);
     };
     if (imageFile) {
       reader.readAsDataURL(imageFile);
     } else {
       setImagePreview(null);
-    };
+    }
   };
 
-  function convertBase() {
+  const convertBase = (file) => {
     const reader = new FileReader();
-    reader.readAsDataURL(selectedFile);
+    reader.readAsDataURL(file);
 
     reader.onload = async () => {
       const base64String = reader.result.split(',')[1];
       setBase64(base64String);
       const returnedData = await returnData(base64String);
       setData(returnedData);
-    }
-  }
+    };
+  };
 
   return (
     <>
       <div className="upload__image-wrapper">
-        { !imagePreview && <label htmlFor="image" className="upload__file-input-label">select an image</label> }
+        {!imagePreview && <label htmlFor="image" className="upload__file-input-label">Select an image</label>}
         <input className="upload__file-input" id="image" type="file" onChange={selectFile} accept="image/*" required />
-        { imagePreview && <img className="upload__image" src={imagePreview} alt="work hard everyday, make few fucking money" /> }
+        {imagePreview && <img className="upload__image" src={imagePreview} alt="work hard everyday, make few fucking money" />}
       </div>
-      <br/>
-      <button className="upload__image-button" onClick={() => { convertBase() }}>convert</button>
+      <br />
       {base64 && !data && <PostSkeleton />}
       {data && <Post analysis={data.analysis} />}
     </>
