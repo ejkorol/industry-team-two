0ffc64e85d853ef2a2c0fb90af02c3d243a7878c
Eo Soo Kim <eosookim@gmail.com>
2024-06-28 14:34:26 -0400

    remove useless code/files

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,7 @@
-import Home from "@/components/Home/Home";
-import NavBar from "@/components/NavBar/NavBar";
 import ImageGen from "@/components/ImageGen/ImageGen";
 import Footer from "../../components/Footer/Footer";
-import Post from "../../components/Post/Post";
 import PostFooter from "../../components/PostFooter/PostFooter";
 import PostHeader from "../../components/PostHeader/PostHeader";
-import Success from "../../components/Success/Success";
 import "./HomePage.scss";
 
 export default function HomePage() {
