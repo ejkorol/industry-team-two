82f8c54bcfe12cdeff6986a6dc562e8f24f439e6
Jason Korol <ejkorol@gmail.com>
2024-06-28 02:06:09 -0400

    add utilities for openai sdk

---
+++ src/utils/prompt.js
@@ -0,0 +1,3 @@
+export function prompt() {
+  return (`I have an image that I need to create a product listing for on a community marketplace. Please provide the following details for the image: A short but descriptive title. A short 160 word simple description. 0-5 tags that represent the image.`)
+};
